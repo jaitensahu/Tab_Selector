8c45f3f46caa8046dc83706629a460fc901af0fa
Jaiten <Jaitensahu9@gmail.com>
2023-10-11 23:34:40 +0530

    added hover effect

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let buttons = document.querySelectorAll("button");
 let para=document.querySelectorAll(".text-container div");
 let image=document.querySelector(".image-container");
 buttons[0].classList.add("selectedButton");
-
 buttons.forEach((val,idx)=>{
     val.addEventListener("click", () => {
         addClass(idx);
@@ -15,8 +14,6 @@ function addClass(idx) {
       buttons[i].classList.remove("selectedButton");
       para[i].style.display = "block";
       image.style.left=`-${100*(i)}%`;
-      // console.log();
-      
     } 
     else{ 
     buttons[i].classList.add("selectedButton");
